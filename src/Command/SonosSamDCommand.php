527f93b67df78b6aebd4a254ae65b43b3e444aa5
Mikey Burton <mikey@fifteen.co.uk>
2017-11-11 15:23:34 +0000

    Add easter egg

---
+++ src/Command/SonosSamDCommand.php
@@ -0,0 +1,30 @@
+<?php
+namespace MadMikeyB\SonosBot\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use duncan3dc\Sonos\Network;
+use duncan3dc\Sonos\Tracks\Spotify;
+
+class SonosSamDCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('sonos:crossroads');
+        $this->setDescription('Play Crossroads by Blazin\' Squad');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $sonos = new Network;
+
+        $controller = $sonos->getController();
+        $track = new Spotify("4IEOKcFZGMiKp5NXYjH001");
+        $controller->useQueue();
+        $controller->getQueue()->addTrack($track, 1);
+        $controller->play();
+    }
+}
